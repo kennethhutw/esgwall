c6a1b555be749a4ed65ced2c0d437d3c09088ed0
Hu Chia Wei <kenneth_hu@livemail.tw>
2023-05-22 22:28:48 +0800

    fixed

--- src/components/shared/navbar.js
+++ src/components/shared/navbar.js
@@ -27,43 +27,6 @@ const LogoutLink = () =>
   <a className="nav-link port-navbar-link" href="/api/v1/logout">Logout</a>
 
 
-const AdminMenu = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  return (
-    <Dropdown
-      className="port-navbar-link port-dropdown-menu"
-      nav
-      isOpen={isOpen}
-      toggle={() => setIsOpen(!isOpen)}>
-        <DropdownToggle className="port-dropdown-toggle" nav caret>
-          Admin
-        </DropdownToggle>
-        <DropdownMenu right>
-          <DropdownItem>
-            <BsNavLink
-              className="port-dropdown-item"
-              href="/portfolios/new"
-              title="Create Portfolio"
-            />
-          </DropdownItem>
-          <DropdownItem>
-            <BsNavLink
-              className="port-dropdown-item"
-              href="/blogs/editor"
-              title="Blog Editor"
-            />
-          </DropdownItem>
-          <DropdownItem>
-            <BsNavLink
-              className="port-dropdown-item"
-              href="/dashboard"
-              title="Dashboard"
-            />
-          </DropdownItem>
-        </DropdownMenu>
-    </Dropdown>
-  )
-}
 
 const Navbar = () => {
   const {user} = useUser();
